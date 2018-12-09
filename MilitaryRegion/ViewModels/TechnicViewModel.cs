0fd2cc2d064148813ea3c6c5c2bdd7048ddbdc58
dimon <dmytro.havryliak@gmail.com>
2018-12-09 21:08:20 +0200

    start of 6th query

---
+++ MilitaryRegion/ViewModels/TechnicViewModel.cs
@@ -0,0 +1,32 @@
+﻿using MilitaryRegion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MilitaryRegion.ViewModels
+{
+    public class TechnicViewModel
+    {
+        // id
+        public int Id { get; set; }
+
+        // ід військової частини
+        public int MilitaryBaseId { get; set; }
+        public string MilitaryBaseName { get; set; }
+
+        // id техніки
+        public int MachineryId { get; set; }
+        public string Name { get; set; }
+        // модель техніки
+        public string Model { get; set; }
+        // вага, тонни
+        public double Weight { get; set; }
+        // кількість екіпажу
+        public int CrewAmount { get; set; }
+
+        // кількість
+        public int Amount { get; set; }
+
+    }
+}
